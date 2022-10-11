4764f76f70659b20f3c9fa5f1112f10f83857eb5
Diana Nóia <Diana_noia@hotmail.com>
2022-10-11 12:06:12 +0100

    Exercicio 5

---
+++ CityManager3/Building.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace CityManager3
+{
+    public class Building : IHasValue, IComparable<IHasValue>
+    {
+        public string Type { get; }
+        public float Value { get; }
+        public float Area { get; }
+
+        public Building(string type, float value, float area)
+        {
+            Type = type;
+            Value = value;
+            Area = area;
+        }
+
+        public override string ToString() =>
+            $"{Type,-20}{Value,8:f2}${Area,8:f2}m2";
+
+        // Método obrigatório devido à classe implementar IHasValue
+        public bool Equals(IHasValue other)
+        {
+            if (other is null) return false;
+            return Value == other.Value;
+        }
+
+        public int CompareTo(IHasValue other)
+        {
+            // Nulls always come first in sorting
+            if (other == null) return 1;
+            if (Value < other.Value) return 1;
+            if (Value > other.Value) return -1;
+            return 0;
+        }
+    }
+}
